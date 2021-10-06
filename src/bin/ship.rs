9728b24c148a23407723b4ac2ab54e458b10d10a
Hongze Xia <hongze.x@gmail.com>
2021-10-06 22:42:24 +1100

    feat: add cmd to ship

--- src/bin/ship.rs
+++ src/bin/ship.rs
@@ -1,3 +1,31 @@
+//! Command line options
+use clap::Clap;
+use std::{thread, time::Duration};
+
+#[derive(Clap, Debug)]
+pub struct CmdOpts {
+    /// remote server address
+    #[clap(short, long)]
+    pub server: String,
+    /// this hostname
+    #[clap(short, long)]
+    pub hostname: Option<String>,
+    /// UUID of this ship
+    #[clap(short, long)]
+    pub uuid: Option<String>,
+    /// heartbeat interval in seconds
+    #[clap(short, long, default_value = "10")]
+    pub interval: u64,
+    /// maximum offline time in seconds at least 3x as large as interval
+    #[clap(short, long, default_value = "30")]
+    pub offline: u64,
+}
+
 fn main() {
-    println!("Hello, beaon!");
+    let opts = CmdOpts::parse();
+
+    loop {
+        thread::sleep(Duration::from_secs(opts.interval));
+        println!("Hello, beacon!");
+    }
 }
