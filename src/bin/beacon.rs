5c720dc32611e33863962851ac6a22c37e67f308
Hongze Xia <hongze.x@gmail.com>
2021-10-09 23:29:24 +1100

    WIP: ship added to hashmap

--- src/bin/beacon.rs
+++ src/bin/beacon.rs
@@ -1,8 +1,14 @@
 mod model;
 use model::ShipAliveReq;
-use rocket::serde::json::Json;
+use rocket::{serde::json::Json, State};
+use std::{collections::HashMap, sync::Mutex, time::SystemTime};
 #[macro_use]
 extern crate rocket;
+
+struct ShipInfo {
+    pub requests: HashMap<String, ShipAliveReq>,
+    pub last_seens: HashMap<String, SystemTime>,
+}
 
 #[get("/")]
 fn hello() -> &'static str {
@@ -10,12 +16,26 @@ fn hello() -> &'static str {
 }
 
 #[post("/ship", format = "application/json", data = "<ship>")]
-fn register_ship(ship: Json<ShipAliveReq>) -> &'static str {
+fn register_ship(ship: Json<ShipAliveReq>, state_data: &State<Mutex<ShipInfo>>) -> &'static str {
     println!("ship: {:?}", ship);
+    let mut ship_info = state_data.lock().unwrap();
+    let uuid = (*ship.uuid).to_string();
+    if ship_info.requests.contains_key(&uuid) {
+        println!("We have got {}", &uuid);
+    } else {
+        println!("Adding ship {:?} to the database", ship);
+        ship_info.requests.insert(uuid.clone(), (*ship).clone());
+        ship_info.last_seens.insert(uuid, SystemTime::now());
+    }
     "ok"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, register_ship])
+    rocket::build()
+        .manage(Mutex::new(ShipInfo {
+            requests: HashMap::new(),
+            last_seens: HashMap::new(),
+        }))
+        .mount("/", routes![hello, register_ship])
 }
--- src/bin/model/mod.rs
+++ src/bin/model/mod.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 // "Ship is alive" request
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ShipAliveReq {
     pub hostname: String,
     pub max_offline: u64,
