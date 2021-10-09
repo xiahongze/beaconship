c9733691a32de84261c194718c97f3a68d1050b3
Hongze Xia <hongze.x@gmail.com>
2021-10-09 16:10:14 +1100

    WIP: added rocket

--- src/bin/beacon.rs
+++ src/bin/beacon.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, ship!");
+mod model;
+use model::ShipAliveReq;
+use rocket::serde::json::Json;
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[post("/ship", format = "application/json", data = "<ship>")]
+fn register_ship(ship: Json<ShipAliveReq>) -> &'static str {
+    println!("ship: {:?}", ship);
+    "ok"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello, register_ship])
 }
